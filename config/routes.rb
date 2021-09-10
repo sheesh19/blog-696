24b8b5b3b070e6a355ee12513f533cba88ff600c
Sheila Leveille <sbleveille@gmail.com>
2021-09-10 11:23:42 +0300

    FEAT: Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root to: 'articles#index'
-  resources :articles, except: :index
+  # root to: 'articles#index'
+  # resources :articles, except: :index
+  resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
